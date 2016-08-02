1ace8aca9004b50f844d9f932137ea51b7b3fced
jerrykuo7727 <jerrykuo7727@gmail.com>
2016-08-03 01:37:47 +0800

    Update RSpec test of board#gameover?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,9 +6,24 @@ describe Board do
   describe "#gameover?" do
     context "given #line_match? being 'true'" do
       it "returns the winner" do
-        turns = double
         allow(board).to receive(:line_match?).and_return(true)
         expect(board.gameover?).to eql(board.turn)
+      end
+    end
+
+    context "given #line_match being true and #tie? being 'true'" do
+      it "returns 'tie'" do
+        allow(board).to receive(:line_match?).and_return(false)
+        allow(board).to receive(:tie?).and_return(true)
+        expect(board.gameover?).to eql("tie")
+      end
+    end
+
+    context "given both #line_match? and #tie? being 'false" do
+      it "returns 'false'" do
+        allow(board).to receive(:line_match?).and_return(false)
+        allow(board).to receive(:tie?).and_return(false)
+        expect(board.gameover?).to eql(false)
       end
     end
   end
